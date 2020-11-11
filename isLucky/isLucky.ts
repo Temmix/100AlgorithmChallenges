bbdfb5619fb3169b36660aa9b9b39fbb9731472a
Temi Makinde <Temi.Makinde@Gmail.com>
2020-11-11 15:01:02 +0000

    some changes

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,15 +1,29 @@
 function isLucky(n: number): boolean {
-    let sumA = 0;
-    let sumB = 0;
-    const nStr = n.toString();
-    const length = nStr.length / 2;
-    const allLength = nStr.length;
-    for(let i = 1; i <= length; i++){
-        sumA += +nStr[i- 1];
-        sumB += +nStr[allLength - i];
-    } 
-    return sumA === sumB;
+  let sumA = 0;
+  let sumB = 0;
+  const nStr = n.toString();
+  const length = nStr.length / 2;
+  const allLength = nStr.length;
+  for (let i = 1; i <= length; i++) {
+    sumA += +nStr[i - 1];
+    sumB += +nStr[allLength - i];
+  }
+  return sumA === sumB;
 }
 
+const isLucky2 = (
+  num: number,
+  list = num.toString().split(""),
+  length = list.length / 2
+): boolean => {
+  return (
+    list.slice(0, 2).reduce((x, y) => +x + +y) ===
+    list.slice(length).reduce((x, y) => +x + +y)
+  );
+};
+
 console.log(isLucky(1230));
+console.log(isLucky2(1230));
+
 console.log(isLucky(239017));
+console.log(isLucky2(239017));
--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,12 +1,13 @@
 function longestDigitsPrefix(inputString: string): string {
-    if(!inputString) return null;
-    
-    let digits = '';
-    const allowedDigits = '0123456789';
-    for(let i = 0; i < inputString.length; i++){
-        if(allowedDigits.indexOf(inputString[i]) !== -1) digits += inputString[i];
-        else break; 
-    } return digits;
+  if (!inputString) return null;
+
+  let digits = "";
+  const allowedDigits = "0123456789";
+  for (let i = 0; i < inputString.length; i++) {
+    if (allowedDigits.indexOf(inputString[i]) !== -1) digits += inputString[i];
+    else break;
+  }
+  return digits;
 }
 
-console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix("123aa1"));
